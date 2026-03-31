018b62ae0af4f910423d192e7d642a258efea576
rdgtech-ctrl <dg.mechpro200@gmail.com>
2026-03-31 15:50:03 +0530

    Update files

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {Provider} from 'react-redux'
+import {store} from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  </React.StrictMode>
 )
---
+++ vite-project/src/redux/features/counterSlice.js
@@ -0,0 +1,7 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+
+})
+
+const [first,setfirst] = useState(second)
