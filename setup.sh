0956737b1039a5010129a9000a08e25eba778f4e
rdgtech-ctrl <dg.mechpro200@gmail.com>
2026-03-31 15:17:16 +0530

    Update files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git init
-git remote add origin " https://github.com/rdgtech-ctrl/redux-toolkit.git"
+git remote add origin "https://github.com/rdgtech-ctrl/redux-toolkit.git"
 git config user.email "dg.mechpro200@gmail.com"
 git config user.name "rdgtech-ctrl"
 echo "Setup complete!"
